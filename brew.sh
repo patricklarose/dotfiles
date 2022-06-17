7721eefb644c3b94b10c427e79cb48b4c0e18af8
Patrick Larose <77698334+patricklarose@users.noreply.github.com>
2022-06-16 23:15:11 -0600

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Install Homebrew | https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Update and upgrade Homebrew
+brew update && brew upgrade
+
+# Install GUI applications | Cask
+brew install cask
+brew install --cask firefox
+brew install --cask visual-studio-code
+brew install --cask audacity
+brew install --cask vlc
+brew install --cask virtualbox
+brew install --cask wireshark
+brew install --cask anki
+
+# Install CLI applications | Formula
+brew install vim
+brew install docker
+brew install gnupg
+brew install wget
+brew install tree
+brew install htop
+brew install neofetch
+brew install exiftool
+brew install nmap
+brew install openvpn
+brew install speedtest-cli
+
+# Install Source Code Pro font
+brew tap homebrew/cask-fonts
+brew install svn
+brew install font-source-code-pro
+
+# Delete old and unused formulas/casks
+brew cleanup
