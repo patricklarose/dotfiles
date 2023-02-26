c9c379f783538f35ec26f3e7cfa86fab2ff7f8d9
Patrick Larose <77698334+patricklarose@users.noreply.github.com>
2023-02-26 11:05:19 -0700

    Optimize script

--- brew.sh
+++ brew.sh
@@ -6,37 +6,29 @@
 # Update and upgrade Homebrew
 brew update && brew upgrade
 
-# Install GUI applications | Cask
-brew install cask
-brew install --cask firefox
-brew install --cask visual-studio-code
-brew install --cask audacity
-brew install --cask vlc
-brew install --cask virtualbox
-brew install --cask wireshark
-brew install --cask anki
-
-# Install CLI applications | Formula
-brew install vim
-brew install docker
-brew install gnupg
-brew install wget
-brew install tree
-brew install htop
-brew install neofetch
-brew install exiftool
-brew install nmap
-brew install openvpn
-brew install speedtest-cli
-
-# Install zsh plugins
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
-
-# Install Source Code Pro font
-brew tap homebrew/cask-fonts
-brew install svn
-brew install font-source-code-pro
+# Install applications
+brew install cask \
+  firefox \
+  visual-studio-code \
+  audacity \
+  vlc \
+  virtualbox \
+  wireshark \
+  anki \
+  vim \
+  docker \
+  gnupg \
+  wget \
+  tree \
+  htop \
+  neofetch \
+  exiftool \
+  nmap \
+  openvpn \
+  speedtest-cli \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting \
+  font-source-code-pro
 
 # Delete old and unused formulas/casks
 brew cleanup
