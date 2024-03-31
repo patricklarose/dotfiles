1ade694ecbf34bfcd255b4d1236fa432e36a4b99
Patrick Larose <77698334+patricklarose@users.noreply.github.com>
2024-03-31 09:06:57 -0700

    Add backup functionality

--- setup.sh
+++ setup.sh
@@ -12,11 +12,20 @@ fi
 # Define the source directory of dotfiles
 dotfiles_dir=$PWD
 
+# Define the backup directory and create it if it doesn't exist
+backup_dir="$HOME/dotfiles_backup"
+mkdir -p "$backup_dir"
+
 # Define the list of dotfiles to manage
 dotfiles=(".zshrc" ".vimrc" ".gitconfig" ".hushlogin" ".macos")
 
 # Loop through the list of dotfiles
 for dotfile in "${dotfiles[@]}"; do
+    # Check if the target file already exists and back it up with .bak extension
+    if [ -f "$HOME/$dotfile" ]; then
+        echo "Backing up existing $dotfile to $backup_dir."
+        mv "$HOME/$dotfile" "$backup_dir/${dotfile}.bak"
+    fi
     # Create a symbolic link in the home directory to the corresponding file in the dotfiles directory
     echo "Linking $dotfile to home directory."
     ln -sf $dotfiles_dir/$dotfile $HOME/$dotfile
