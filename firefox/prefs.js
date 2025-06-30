e3d2f9644c2c93a1d71dd2724066230070b5fc58
Patrick Larose <77698334+patricklarose@users.noreply.github.com>
2025-06-29 21:35:39 -0700

    Disable “remember password” prompts

--- firefox/prefs.js
+++ firefox/prefs.js
@@ -83,6 +83,7 @@ user_pref("privacy.usercontext.about_newtab_segregation.enabled", true);
 user_pref("security.ssl.disable_session_identifiers", true);
 user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSite", false);
 user_pref("signon.autofillForms", false);
+user_pref("signon.rememberSignons", false);
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("toolkit.telemetry.bhrPing.enabled", false);
 user_pref("toolkit.telemetry.cachedClientID", "");
